6eb9ede55bf06cb7c87e62ef441fa469722875da
CodeDepo <codedepon@gmail.com>
2023-03-27 16:17:12 +0200

    Cors and logout

--- src/main/java/com/authprovider/config/ApplicationSecurity.java
+++ src/main/java/com/authprovider/config/ApplicationSecurity.java
@@ -1,8 +1,13 @@
 package com.authprovider.config;
 
+import com.authprovider.dto.SecureCookie;
+import jakarta.servlet.http.Cookie;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,9 +15,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurity {
 
   @Bean
@@ -21,11 +34,26 @@ public class ApplicationSecurity {
   }
 
   @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins("http://localhost:3000")
+          .allowCredentials(true)
+          .allowedMethods("GET", "POST", "DELETE");
+      }
+    };
+  }
+
+  @Bean
   public SecurityFilterChain filterChain(
     HttpSecurity http,
     @Autowired JwtAuthorizationFilter jwtAuthorizationFilter
   ) throws Exception {
     http.csrf().disable();
+    http.cors();
     http
       .sessionManagement()
       .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -33,9 +61,10 @@ public class ApplicationSecurity {
       .authorizeHttpRequests()
       .requestMatchers(
         "/api/auth/**",
-        "/api/roles",
-        "/api/test",
+        // "/api/roles",
+        // "/api/test",
         "/api/oauth2/token"
+        // "/api/oauth2/authorize"
       )
       .permitAll()
       .anyRequest()
@@ -45,6 +74,25 @@ public class ApplicationSecurity {
         jwtAuthorizationFilter,
         UsernamePasswordAuthenticationFilter.class
       );
+
+    http.logout(logout ->
+      logout
+        .logoutUrl("/api/auth/logout")
+        .addLogoutHandler(
+          new CookieClearingLogoutHandler(
+            SecureCookie.delete(SecureCookie.accessTokenKey),
+            SecureCookie.delete(SecureCookie.refreshTokenKey)
+          )
+        )
+        .logoutSuccessHandler(
+          (new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT))
+        )
+        .deleteCookies(
+          SecureCookie.accessTokenKey,
+          SecureCookie.refreshTokenKey
+        )
+        .logoutSuccessUrl("http://localhost:3000")
+    );
 
     return http.build();
   }
